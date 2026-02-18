28a28d01259421af4e2f5a1dc50b8e1cb2a9245c
reismaa <apriliandv07@gmail.com>
2026-02-18 11:04:10 +0700

    add new file

--- JOBSHEET2/MahasiswaMain24.java
+++ JOBSHEET2/MahasiswaMain24.java
@@ -4,17 +4,18 @@ public class MahasiswaMain24 {
     public static void main(String[] args) {
 
         Mahasiswa24 mhs1 = new Mahasiswa24();
-
         mhs1.nama = "Muhammad Ali Farhan";
         mhs1.nim = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk = 3.55;
 
         mhs1.tampilkanInformasi();
-
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
-        
         mhs1.tampilkanInformasi();
+
+        Mahasiswa24 mhs2 = new Mahasiswa24("Annisa Nabila","2141720106", 3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
