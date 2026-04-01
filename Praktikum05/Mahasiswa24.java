a697992c285f257994c4861b44f59e6e17ac9353
reismaa <apriliandv07@gmail.com>
2026-04-01 09:26:27 +0700

    add new files praktikum 2

---
+++ Praktikum05/Mahasiswa24.java
@@ -0,0 +1,28 @@
+package Praktikum05;
+
+public class Mahasiswa24 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    // Konstruktor default
+    Mahasiswa24() {
+    }
+    
+    // Konstruktor berparameter (dibuat ada yang nama var parameter inputnya sama ada yang tidak)
+    Mahasiswa24(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        ipk = ip;
+        kelas = kls;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+}
