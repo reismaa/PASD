08c2c205f9ea5b2d8bac9e54be5ac21002fa3d7e
reismaa <apriliandv07@gmail.com>
2026-04-01 08:59:17 +0700

    insertionsort

--- Praktikum05/SortingMain24.java
+++ Praktikum05/SortingMain24.java
@@ -24,5 +24,15 @@ public class SortingMain24 {
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
 
+        int c[]= {40, 10, 4, 9, 3};
+
+        Sorting24 dataurut3 = new Sorting24(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
+
     }
 }
