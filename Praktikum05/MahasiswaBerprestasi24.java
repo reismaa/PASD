1bd0fb4808686af009e634adc224f9ad5ad80b7c
reismaa <apriliandv07@gmail.com>
2026-04-01 10:42:31 +0700

    insertionsort descending

--- Praktikum05/MahasiswaBerprestasi24.java
+++ Praktikum05/MahasiswaBerprestasi24.java
@@ -56,7 +56,7 @@ public class MahasiswaBerprestasi24 {
         for (int i=1; i<listMhs.length; i++) {
             Mahasiswa24 temp = listMhs[i];
             int j=i;
-            while (j>0 && listMhs[j-1].ipk>temp.ipk) {
+            while (j>0 && listMhs[j-1].ipk<temp.ipk) {
                 listMhs[j]=listMhs[j-1];
                 j--;
             }
