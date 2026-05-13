bd9f7d861f1c43b3f3542d6312ac31cefe89e946
reismaa <apriliandv07@gmail.com>
2026-05-13 16:06:07 +0700

    jobsheet12

---
+++ Jobsheet12/DoubleLinkedList24.java
@@ -0,0 +1,128 @@
+package Jobsheet12;
+
+public class DoubleLinkedList24 {
+    Node24 head;
+    Node24 tail;
+
+    public DoubleLinkedList24() {
+        head = null;
+        tail = null;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    public void addLast(Mahasiswa24 data) {
+        Node24 newNode = new Node24(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public void insertAfter(String keyNim, Mahasiswa24 data) {
+        Node24 current = head;
+        while (current != null && !current.data.nim.equals(keyNim)) {
+            current = current.next;
+        }
+
+        if (current == null) {
+            System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
+            return;
+        }
+
+        Node24 newNode = new Node24(data);
+
+        // Jika current adalah tail, cukup tambahkan di akhir
+        if (current == tail) {
+            current.next = newNode;
+            newNode.prev = current;
+            tail = newNode;
+        } else {
+            // Sisipkan di tengah
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong.");
+            return;
+        }
+        Mahasiswa24 dataHapus = head.data;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+
+        System.out.println("Data berhasil dihapus.");
+        dataHapus.tampil();
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked List kosong.");
+            return;
+        }
+        Mahasiswa24 dataHapus = tail.data;
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+
+        System.out.println("Data berhasil dihapus.");
+        dataHapus.tampil();
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong.");
+            return;
+        }
+
+        Node24 current = head;
+        while (current != null) {
+            current.data.tampil();
+            current = current.next;
+        }
+    }
+
+    public void printReverse() {
+        if (isEmpty()) {
+            System.out.println("Linked List masih kosong.");
+            return;
+        }
+
+        Node24 current = tail;
+
+        System.out.println("Data Mahasiswa (Reverse):");
+        while (current != null) {
+            current.data.tampil();
+            current = current.prev;
+        }
+    }
+}
---
+++ Jobsheet12/Node24.java
@@ -0,0 +1,13 @@
+package Jobsheet12;
+
+public class Node24 {
+    Mahasiswa24 data;
+    Node24 prev;
+    Node24 next;
+
+    public Node24(Mahasiswa24 data) {
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+    }
+}
