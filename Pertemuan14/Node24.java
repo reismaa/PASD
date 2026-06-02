2d9c42f3322b211015e976ceb7be312a43f6d70a
reismaa <apriliandv07@gmail.com>
2026-06-02 11:36:14 +0700

    add new files

---
+++ Pertemuan14/Node24.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node24 {
+    Mahasiswa24 mahasiswa;
+    Node24 left, right;
+
+    public Node24() {
+    }
+
+    public Node24(Mahasiswa24 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+    
+}
